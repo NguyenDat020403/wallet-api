85eae7b0422a5ab85372bc1d5ce75c6ca25a3cc0
DatNDT <trinhdat0123@gmail.com>
2025-05-09 10:36:25 +0700

    b

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import Moralis from 'moralis';
 
 async function bootstrap() {
-  await Moralis.start({
-    apiKey: process.env.MORALIS_API,
-  });
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  await app.listen(3333);
-  // await app.listen(process.env.PORT ?? 3333);
+  const PORT = process.env.PORT || 3333;
+  await app.listen(PORT, '0.0.0.0');
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
