eb7a370e292cee2bab184d2993b0f0004cf412f9
DatNDT <trinhdat0123@gmail.com>
2025-05-19 19:29:44 +0700

    Fix

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -1,16 +1,12 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as admin from 'firebase-admin';
-import path from 'path';
 const config = new ConfigService();
 
 @Injectable()
 export class FirebaseService implements OnModuleInit {
   onModuleInit() {
-    const firebaseCredPath =
-      config.get<string>('FIREBASE_CREDENTIALS_PATH') ||
-      'src/config/firebase-adminsdk.json';
-    const serviceAccount = require(path.resolve(firebaseCredPath));
+    const serviceAccount = config.get('FIREBASE_CONFIG');
 
     if (!admin.apps.length) {
       admin.initializeApp({
@@ -45,14 +41,7 @@ export class FirebaseService implements OnModuleInit {
       console.log('Success:', res);
       return res;
     } catch (err) {
-      console.error('❌ Error sending notification:', err.code, err.message);
-
-      // Nếu token không hợp lệ
-      if (err.code === 'messaging/registration-token-not-registered') {
-        // Option: gọi prisma để xóa token tương ứng trong bảng users
-        console.warn('⚠️ Token is not registered. Should remove from DB.');
-      }
-
+      console.error('❌ Error sending notification:', err);
       throw err;
     }
   }
